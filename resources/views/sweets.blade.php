082cb65f6016a47273592c1852f45572bbfe1896
ShilpaPaul <57042771+ShilpaPaul@users.noreply.github.com>
2021-05-31 23:37:48 +0530

    Update sweets.blade.php

--- resources/views/sweets.blade.php
+++ resources/views/sweets.blade.php
@@ -9,7 +9,7 @@
   <img src="https://cdn.igp.com/f_auto,q_auto,t_prodm/products/p-elegant-rose-bouquet-139330-m.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <p class="card-text">
-    <h5>Fresh Rose Bouquet</h5>Separate the egg yolks from the whites.
+    <h5>Fresh Rose Bouquet</h5>
 The Soulfulness of Nur Jahan Arrangement- A regal symbol of grace and dignity, this flower arrangement is an ode to the inspiring beauty of Nur Jahan, a significant empress of the Mughal Dynasty. The best token of admiration and appreciation to the beautiful and royal women like her, this royal arrangement of pink roses along with the beautiful white fillers is sure to create a bright spot in your special one’s day..</p>
   </div>
 </div>
@@ -41,18 +41,12 @@ The Soulfulness of Nur Jahan Arrangement- A regal symbol of grace and dignity, t
 <br>
 <div class="col col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3">
 <div class="card" style="width: 18rem;">
-  <img src="https://lh3.googleusercontent.com/m5BcF9iCWrDHlP538s1qAAwDRHslKYrZJUCLwvPqZtwhBwT8bJLJ5FDWyDptqUnJnkZy-Y8F0zN_VYdCxmxPjA=s640-c-rw-v1-e365" class="card-img-top" alt="...">
+  <img src="https://www.bloommagicflowers.ie/assets/uploads/vendor/28/products/k255-product-image.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     
-    <p class="card-text"><h5>Iced Strawberry Dessert</h5>
-    Line bowls with parchment paper up the sides, about 2 cm (approximately 3/4 inches).
-Mince strawberries, add lemon juice and set aside. In a bowl, beat egg whites until foamy.
-Heat bowl in a water bath, and continue to whisk while adding the sugar until stiff.
-Pass strawberries through a sieve. Beat cream until stiff and mix with strawberries and egg white mixture.
-Distribute mixture evenly into prepared cups.
-Freeze for about 6 hours.
-Remove from freezer about 15 minutes before serving.
-Garnish with fresh strawberries.</p>
+    <p class="card-text"><h5>Tulip Bouquet</h5>
+       The Soulfulness of Nur Jahan Arrangement- A regal symbol of grace and dignity, this flower arrangement is an ode to the inspiring beauty of Nur Jahan, a significant empress of the Mughal Dynasty. The best token of admiration and appreciation to the beautiful and royal women like her, this royal arrangement of pink roses along with the beautiful white fillers is sure to create a bright spot in your special one’s day.
+.</p>
   </div>
 </div>
 </div>
